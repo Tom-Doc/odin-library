cc3bf1296c771190368b45b5ac776d24fe9daa98
Tom <Tomdocherty1@gmail.com>
2023-05-04 18:14:00 -0700

    Added documnet query selectors for Add Book button, and book form. Added started TOP code, and started event listener for Add Book button

---
+++ script.js
@@ -0,0 +1,16 @@
+const addBookBtn = document.querySelector(".add-book-btn");
+const newBookModal = document.querySelector(".book-form");
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+addBookBtn.addEventListener("click", () => {
+  newBookModal.style.display = "block";
+});
