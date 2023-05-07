85b859ece2c21bc1f6815fac73284e7d4adad56a
Tom <Tomdocherty1@gmail.com>
2023-05-07 08:44:21 -0700

    Finished addBookToLibrary function, added bookFormRows, added subMitBookBtn

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const modal = document.querySelector(".modal");
 const addBookBtn = document.querySelector(".add-book-btn");
 const closeModalBtn = document.querySelector(".close-modal-btn");
-// const newBookModal = document.querySelector(".book-form");
+const submitBookBtn = document.querySelector(".submit-book-btn");
+const bookFormRows = document.querySelectorAll(".form-row");
 
 let myLibrary = [];
 
@@ -13,11 +14,31 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").value;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
 }
 
 addBookBtn.addEventListener("click", () => {
   modal.showModal();
+});
+
+submitBookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  modal.close();
+
+  // reset input fields
+  bookFormRows.forEach((row) => {
+    const input = row.querySelector("input");
+    if (input) input.value = "";
+  });
+  const readSelect = document.querySelector("#read"); //reset the select/dropdown option
+  readSelect.selectedIndex = 0;
 });
 
 closeModalBtn.addEventListener("click", () => {
