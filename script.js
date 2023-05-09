3bb45b2107df8203fc8bde418a9532143fbbc77b
Tom <Tomdocherty1@gmail.com>
2023-05-09 16:32:42 -0700

    Added displayBooks function. Added forEach to addBookToLib function to reenable indiv-book styling for each book added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addBookBtn = document.querySelector(".add-book-btn");
 const closeModalBtn = document.querySelector(".close-modal-btn");
 const submitBookBtn = document.querySelector(".submit-book-btn");
 const bookFormRows = document.querySelectorAll(".form-row");
+const bookBody = document.querySelector(".book-list-section");
 
 let myLibrary = [];
 
@@ -17,10 +18,41 @@ function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
-  let read = document.querySelector("#read").value;
+  let read = document.querySelector("#read").value.toUpperCase();
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   console.log(myLibrary);
+  displayBooks();
+  document
+    .querySelectorAll(".individual-book")
+    .forEach((book) => (book.style.display = "block"));
+}
+
+function displayBooks() {
+  bookBody.innerHTML = ""; // Clear existing books
+
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("individual-book");
+
+    const title = document.createElement("p");
+    title.textContent = `Book: ${book.title}`;
+    bookDiv.appendChild(title);
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
+    bookDiv.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
+    bookDiv.appendChild(pages);
+
+    const read = document.createElement("p");
+    read.textContent = `Read: ${book.read}`;
+    bookDiv.appendChild(read);
+
+    bookBody.appendChild(bookDiv);
+  });
 }
 
 addBookBtn.addEventListener("click", () => {
